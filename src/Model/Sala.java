f754c1aeca3130fd8e7950a4aa3f4d03452c75ef
Alice de Oliveira Andalécio Costa <aliceoliveiraacosta@gmail.com>
2017-10-08 16:33:51 -0300

    Implementando as classes Usuario e Sala

--- src/Model/Sala.java
+++ src/Model/Sala.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Model;
 
-/**
- *
- * @author Alice
- */
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class Sala {
+
+    private final IntegerProperty idSala;
+    private final StringProperty desSala; //descrição
+
+    public Sala() {
+        this(null, null);
+    }
+
+    public Sala(Integer idSala, String desSala) {
+        this.idSala = new SimpleIntegerProperty(idSala);
+        this.desSala = new SimpleStringProperty(desSala);
+    }
+
+    public Integer getIdSala() {
+        return idSala.get();
+    }
     
+    public void setIdSala(Integer idSala) {
+        this.idSala.set(idSala);
+    }
+    
+    public IntegerProperty idSalaProperty() {
+        return idSala;
+    }
+
+    public String getDesSala() {
+        return desSala.get();
+    }
+    
+    public void setDesSala(String desSala) {
+        this.desSala.set(desSala);
+    }
+    
+    public StringProperty desSalaProperty() {
+        return desSala;
+    }
 }
